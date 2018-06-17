8a1a358f2b2d15efa34241a8f98bc2cadc6d1be3
Kassaoui <kaissaoui@mytranslation.com>
2018-06-17 22:28:38 +0200

    lambda page

--- hook.user.js
+++ hook.user.js
@@ -9,21 +9,102 @@ console.log("Loading hook")
 
 var sidebarLambdaComponent =
   ' \
-<li style="cursor:pointer" ng-class="{active: $state.includes(\'lambda\'), disabled: $state.includes(\'warnings\')}"> \
-    <a id="lambda" ui-sref="lambda.list({\'zoneId\': currentZone.id})" ui-sref-opts="{reload: true}"> \
+<li style="cursor:pointer"> \
+    <a id="lambda"> \
         <div class="actidiv" style="margin-right:3px"></div> \
         <i style="-webkit-text-fill-color: white;-webkit-text-stroke-color: #704384;-webkit-text-stroke-width: 1.2px" class="fas fa-2x fa-bolt"></i> \
-        Executors \
+        Lambda \
     </a> \
 </li>';
 
 var fontAwesome = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" crossorigin="anonymous">'
 
-$(document)
-$(".main-view").on("change", function(e) {
-  alert("changed")
-});
+var lambdaModal = '<div id="lambdaModal" class="modal fade" role="dialog">\
+  <div class="modal-dialog">\
+    <!-- Modal content-->\
+    <div class="modal-content">\
+      <div class="modal-header">\
+        <h4 class="modal-title">Create lambda</h4>\
+      </div>\
+      <div class="modal-body">'
++
+      '<div class="col-xs-12 col-sm-12 nopadding ng-scope">\
+          <div class="center">\
+              <form>\
+                <div class="form-group">\
+                  <label for="code-typeLabel">code-type</label>\
+                  <select class="form-control" id="code-type">\
+                    <option>csharp</option>\
+                    <option>go</option>\
+                    <option>node</option>\
+                    <option>python</option>\
+                    <option>python3</option>\
+                    <option>ruby</option>\
+                  </select>\
+                </div>\
+                <div class="form-group">\
+                  <label for="exampleInputPassword1">Name</label>\
+                  <input type="text" class="form-control" id="codeName" placeholder="name">\
+                </div>\
+                <div class="form-group">\
+                  <label for="exampleFormControlFile1">file input</label>\
+                  <input type="file" class="form-control-file" id="file">\
+                </div>\
+                <button type="submit" class="btn btn-primary">Submit</button>\
+              </form>\
+          </div>\
+      </div>'
++
+      '</div>\
+      <div class="modal-footer">\
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\
+      </div>\
+    </div>\
+  </div>\
+</div>';
 
+var lambdaBodyStart ='\
+<div class="col-xs-12 col-sm-12 nopadding ng-scope">\
+  <div class="row page-header ng-scope">\
+    <div class="col-md-8 nopadding">\
+      <h1 class="ng-binding">Lambda</h1>\
+    </div>\
+    <div class="col-md-4 pull-right search-field nopadding">\
+      <div class="input-group input-group-sm search-group"><span class="input-group-btn">\
+      <button type="button" class="btn btn-default"><i class="fa fa-search fa-large"></i></button></span>\
+      <input type="text" placeholder="Search bucket" class="form-control ng-pristine ng-untouched ng-valid">\
+      </div>\
+    </div>\
+  </div>\
+  <div class="row button-bar ng-scope">\
+    <div class="col-md-8 nopadding"></div>\
+    <div class="col-md-4 nopadding">\
+      <button id="create-lambda" class="btn btn-primary btn-xl pull-right ng-binding" data-toggle="modal" data-target="#lambdaModal" > Create lambda</button>\
+    </div>\
+  </div>\
+  <div class="row ng-scope">\
+    <div class="col-md-12 nopadding">\
+      <table class="table ng-scope ng-table">\
+        <thead ng-include="templates.header" class="ng-scope">\
+          <tr class="ng-scope">\
+          <th class="ng-binding ng-scope">Name</th>\
+        </thead>\
+        <tbody class="ng-scope">';
+var lambdaBodyEnd =
+        '</tbody>\
+      </table>\
+    </div>\
+  </div>\
+</div>\
+';
+
+var genLambdaBody = function(list){
+    var tableContent = "";
+    for (var i = list.length - 1; i >= 0; i--) {
+        tableContent = tableContent.concat('<tr class="ng-table-group"><td width="100">'+list[i]+'</td></tr>');
+    }
+    return lambdaBodyStart + tableContent + lambdaBodyEnd;
+}
 
 function loadFont() {
     $("head").append(fontAwesome)
@@ -35,7 +116,9 @@ function sideLambda() {
     $("#lambda").click(function (e) {
         e.preventDefault();
         e.stopPropagation();
-        alert("got it")
+        window.history.pushState("", "", '/#/lambda');
+        $("div[ui-view='']").html(genLambdaBody(["name 1", "name 2", "name 3"]));
+        $("body").append(lambdaModal);
     })
 }
 
